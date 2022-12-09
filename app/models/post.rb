79898dab14635b6ca51cf063a3cae0087d8538e6
melashu <meshu102@outlook.com>
2022-12-09 14:18:34 +0300

    Add validation for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: :User, foreign_key: :author_id
   has_many :comments
   has_many :likes
-
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comment_counter, :like_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   def recent_five_comment
     Comment.last(5).length
   end
