5185a11370e08decccafb9e14dea0f2fc21df3e6
melashu <meshu102@outlook.com>
2022-12-13 09:46:52 +0300

    Add linter fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_07_130713) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_09_195150) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,7 +35,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_07_130713) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "string"
     t.text "text"
     t.integer "comment_counter"
     t.integer "like_counter"
