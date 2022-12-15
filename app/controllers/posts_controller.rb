69f4a95551be1df0ed48d5180a085046d9bd651f
melashu <meshu102@outlook.com>
2022-12-15 13:00:51 +0300

    Add linter fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
   def index
     @user_id = params[:user_id]
+    @user = User.where(id: @user_id)
   end
 
   def show
     @user = params[:user_id]
     @post_id = params[:id]
+    @post = Post.find_by(id: @post_id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @user = User.all
+    @author = User.all
   end
 
   def show
     @user_id = params[:id]
+    @user = User.where(id: @user_id)
   end
 end
