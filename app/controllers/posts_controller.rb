4c183caa751b1dcb316bee554fcb7dcb046471f8
melashu <meshu102@outlook.com>
2022-12-17 10:43:19 +0300

    Add linter fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,34 @@ class PostsController < ApplicationController
     @user = User.find_by(id: params[:user_id])
   end
 
+  # posts/new
   def show
     @user = params[:user_id]
     @post_id = params[:id]
     @post = Post.find_by(id: @post_id)
+  end
+
+  def new
+    render :new, locals: { user: current_user }
+  end
+
+  def create
+    check_param
+    title = params[:user][:title]
+    text = params[:user][:text]
+    post = Post.new(title:, text:, comment_counter: 0, like_counter: 0, author: current_user)
+    if post.save
+      flash[:success] = 'Post created successfuly!'
+      redirect_to user_posts_path
+    else
+      flash.now[:error] = 'Something went wrong!'
+      render :new, locals: { user: current_user }
+    end
+  end
+
+  private
+
+  def check_param
+    params.require(:user).permit(:title, :text)
   end
 end
