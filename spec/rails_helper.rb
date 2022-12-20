0dfe5df1df60098c0eb5fe5713910dbd9a6cdb52
melashu <meshu102@outlook.com>
2022-12-20 11:53:55 +0300

    Add configration file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'capybara/rails'
+require 'capybara/rspec'
+# require 'capybara/rails'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
