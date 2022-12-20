b1a335a0943bbde9174916c8b2e02114039221e3
melashu <meshu102@outlook.com>
2022-12-20 20:53:43 +0300

    Add Linter fix

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -3,9 +3,13 @@ require 'rails_helper'
 describe User, type: :feature do
   before :all do
     @user1 = User.create(name: 'John',
-                         photo: 'https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60', bio: 'Here my story', posts_counter: 3)
+                         photo: 'https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+                         bio: 'Here my story',
+                         posts_counter: 3)
     @user2 = User.create(name: 'meshu',
-                         photo: 'https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60', bio: 'Here my story', posts_counter: 0)
+                         photo: 'https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+                         bio: 'Here my story',
+                         posts_counter: 0)
   end
 
   it 'Should have user name John' do
@@ -14,22 +18,14 @@ describe User, type: :feature do
     expect(page).to have_content('meshu')
   end
 
-    it 'When I click on a user, I am redirected to that users show page.' do
-         visit users_path 
-         
-         all('.user-link').each do |user|
-            user.click
-            expect(page).to have_content('John')
-            expect(current_path).to eq user_path(@user1)
-            expect(page).to have_content(@user1.bio)
-         end
+  it 'When I click on a user, I am redirected to that users show page.' do
+    visit user_path(@user1)
+    click_link @user1.name.to_s
+    expect(current_path).to eq user_path(@user1)
+  end
 
-    end
-
-
-    it 'I can see the number of posts each user has written.' do
-        visit users_path
-        expect(page).to have_content ('Number of posts: 3')
-    end
-
+  it 'I can see the number of posts each user has written.' do
+    visit users_path
+    expect(page).to have_content('Number of posts: 3')
+  end
 end
