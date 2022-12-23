d48bffd275bbab54af361ba9206a8eef142baf58
melashu <meshu102@outlook.com>
2022-12-23 22:43:58 +0300

    Remove unwanted comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,17 +6,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
   end
-  # def destroy
-  #   @user = User.find(params[:user_id])
-  #   @post = Post.find_by(author:@user, id: params[:id])
-
-  #   if @post.destroy
-  #     flash[:success] = "User deleted"
-  #     redirect_to users_path
-  #   else
-  #     flash[:error] = "Error deleting user"
-  #     redirect_to users_path
-  #   end
-
-  # end
 end
