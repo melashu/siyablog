e2f0bb381797a073b1d2d661010394342039434d
melashu <meshu102@outlook.com>
2022-12-23 20:11:28 +0300

    Add cancancan ability

---
+++ app/models/ability.rb
@@ -0,0 +1,17 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    # Define abilities for the user here. For example:
+    can :read, Post
+    return unless user.present?
+
+    can :destroy, Post, author: user
+    can :destroy, Comment, author: user
+    can :create, Comment
+    return unless user.admin?
+
+    can :destroy, Post
+    can :destroy, Comment
+  end
+end
