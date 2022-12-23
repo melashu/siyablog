151f0fbe9d81fc3f36ed2180a9bb20f4b9c7d950
melashu <meshu102@outlook.com>
2022-12-23 22:44:28 +0300

    Add after_destroy call back

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,10 +3,16 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   after_save :update_comment_counter
+  after_destroy :update_comment_counter_after_destroy
 
   private
 
   def update_comment_counter
     post.increment!(:comment_counter)
   end
+  
+  def update_comment_counter_after_destroy
+    post.decrement!(:comment_counter)
+  end
+
 end
