700e9dae1daf795f7424a3c459a4a2394a6c20e4
melashu <meshu102@outlook.com>
2022-12-24 19:12:09 +0300

    Add jwt_authenticatable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+  # include Devise::JWT::RevocationStrategies::Denylist
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable
+         :confirmable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
   has_many :likes, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
   has_many :posts, foreign_key: :author_id
@@ -11,6 +14,7 @@ class User < ApplicationRecord
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   enum status: { draft: 0, completed: 1, published: 2 }
   has_one_attached :photo
+
 
   def recent_post
     posts.order(created_at: :desc).limit(3)
